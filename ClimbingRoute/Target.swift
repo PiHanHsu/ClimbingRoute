cd22fb62f9ba20e8ee609fdb5c31c181e793cc56
PiHan Hsu <pihanhsu@gmail.com>
2016-10-26 17:44:31 +0800

    Add new files and link with Storyboard

---
+++ ClimbingRoute/Target.swift
@@ -0,0 +1,13 @@
+//
+//  Target.swift
+//  ClimbingRoute
+//
+//  Created by PiHan Hsu on 26/10/2016.
+//  Copyright © 2016 PiHan Hsu. All rights reserved.
+//
+
+import UIKit
+
+class Target: NSObject {
+
+}
